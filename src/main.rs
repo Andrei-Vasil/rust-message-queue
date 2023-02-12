74b638b44a2401747838c83a254a315fc1842079
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-02-12 13:10:13 +0200

    add thread handler

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,12 @@ use std::{sync::{Mutex, Arc}, thread};
 use queue::Queue;
 
 fn main() {
-    let mut handles = vec![];
-
-    let q = Arc::new(Mutex::new(Queue::new()));
-    
-    // let q1 = q.clone();
-    // let handle = thread::spawn(move || {
-    //     q1.lock().unwrap().push(1);
-    // });
-    // handles.push(handle);
-
-    let q2 = q.clone();
-    let handle = thread::spawn(move || {
-        q2.lock().unwrap().push(1);
-        q2.lock().unwrap().push(2);
-    });
-    handles.push(handle);
-
-    let q3 = q.clone();
-    let handle = thread::spawn(move || {
-        q3.lock().unwrap().push(3);
-    });
-    handles.push(handle);
-
-
-    // let q4 = q.clone();
-    // let handle = thread::spawn(move || {
-    //     println!("{:?}", q4.lock().unwrap().pop());
-    // });
-    // handles.push(handle);
-
-    let q5 = q.clone();
-    let handle = thread::spawn(move || {
-        println!("{:?}", q5.lock().unwrap().pop());
-        println!("{:?}", q5.lock().unwrap().pop());
-        println!("{:?}", q5.lock().unwrap().pop());
-        println!("{:?}", q5.lock().unwrap().pop());
-    });
-    handles.push(handle);
-
-    // let q6 = q.clone();
-    // let handle = thread::spawn(move || {
-    //     println!("{:?}", q6.lock().unwrap().pop());
-    // });
-    // handles.push(handle);
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    let mut q = Queue::new();    
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    println!("{:?}", q.pop());
+    println!("{:?}", q.pop());
+    println!("{:?}", q.pop());
+    println!("{:?}", q.pop());
 }
--- src/queue.rs
+++ src/queue.rs
@@ -1,43 +1,92 @@
 use std::{
     thread, 
-    sync::{Arc, Mutex, mpsc}, collections::VecDeque
+    sync::{Arc, Mutex, mpsc::{self, Receiver, Sender}}, collections::VecDeque
 };
 
 pub struct Queue {
     max_worker_id: usize,
-    listeners: Vec<Worker>,
+    workers: Arc<Mutex<Vec<(Worker, Receiver<bool>)>>>,
     message_queue: Arc<Mutex<VecDeque<i32>>>,
+    daemon_tx: Sender<bool>,
+    daemon_handle: Option<thread::JoinHandle<()>>,
 }
 
 impl Queue {
     pub fn new() -> Self {
-        Self {
+        let (tx, rx) = mpsc::channel();
+        let mut queue = Self {
             max_worker_id: 0,
-            listeners: Vec::new(),
+            workers: Arc::new(Mutex::new(Vec::new())),
             message_queue: Arc::new(Mutex::new(VecDeque::new())),
-        }
+            daemon_tx: tx,
+            daemon_handle: None,
+        };
+        queue.daemon_handle = queue.init_daemon_thread(rx);
+        queue
+    }
+
+    pub fn init_daemon_thread(&mut self, rx: Receiver<bool>) -> Option<thread::JoinHandle<()>> {
+        let workers_clone = self.workers.clone();
+        Some(thread::spawn(move || loop {
+            match rx.try_recv() {
+                Ok(_) => {
+                    break;
+                },
+                Err(_) => {},
+            };
+
+            let mut guard = workers_clone.lock().unwrap();
+            let workers = &mut *guard;
+            workers.retain(|(worker, rx)| {
+                match rx.try_recv() {
+                    Ok(_) => {
+                        drop(worker);
+                        false
+                    },
+                    Err(_) =>true,
+                }
+            });
+        }))
     }
 
     pub fn push(&mut self, message: i32) {
         let message_queue = self.message_queue.clone();
-        self.listeners.push(Worker::new(
-            self.max_worker_id, 
-            move || {
-                let mut handle = message_queue.lock().unwrap();
-                handle.push_back(message);
-            },
+        let (tx_thread_handler, rx_thread_handler) = mpsc::channel();
+
+        self.max_worker_id += 1;
+        let id = self.max_worker_id;
+        self.workers.lock().unwrap().push((
+            Worker::new(
+                self.max_worker_id, 
+                move || {
+                    println!("{:?}", id);
+                    let mut handle = message_queue.lock().unwrap();
+                    handle.push_back(message);
+                    tx_thread_handler.send(true);
+                },
+            ), 
+            rx_thread_handler,
         ));
     }
 
     pub fn pop(&mut self) -> Option<i32> {
         let message_queue = self.message_queue.clone();
+        let (tx_thread_handler, rx_thread_handler) = mpsc::channel();
         let (tx, rx) = mpsc::channel();
-        self.listeners.push(Worker::new(
-            self.max_worker_id, 
-            move || {
-                let mut handle = message_queue.lock().unwrap();
-                tx.send(handle.pop_front());
-            },
+        
+        self.max_worker_id += 1;
+        let id = self.max_worker_id;
+        self.workers.lock().unwrap().push((
+            Worker::new(
+                self.max_worker_id, 
+                move || {
+                    println!("{:?}", id);
+                    let mut handle = message_queue.lock().unwrap();
+                    tx.send(handle.pop_front());
+                    tx_thread_handler.send(true);
+                },
+            ),
+            rx_thread_handler,
         ));
         rx.recv().unwrap()
     }
@@ -45,7 +94,10 @@ impl Queue {
 
 impl Drop for Queue {
     fn drop(&mut self) {
-        // println!("{:?}", self.message_queue.lock().unwrap());
+        self.daemon_tx.send(true);
+        if let Some(thread) = self.daemon_handle.take() {
+            thread.join().unwrap();
+        }
     }
 }
 
